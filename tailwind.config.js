84cc9efe4b782b6dc0199e3e86531a85429699e8
1asfand <masfandyar44@gmail.com>
2025-11-15 22:41:47 +0500

    feat: add MovieCard and SearchBar components with styling and functionality
    - Implemented MovieCard component to display movie details with navigation.
    - Created SearchBar component for user input with icon integration.
    - Added icons and images constants for better asset management.
    - Defined Movie and MovieDetails interfaces for type safety.
    - Configured Babel and Metro for NativeWind integration.
    - Developed API service for fetching movies and movie details from TMDB.
    - Introduced custom hook useFetch for data fetching and state management.
    - Set up Tailwind CSS configuration with custom theme colors.
    - Added TypeScript declarations for image imports.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const theme = require("./themes").default;
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      ...theme,
+    },
+  },
+  plugins: [],
+}
---
+++ themes/colors.ts
@@ -0,0 +1,8 @@
+const colors = {
+    primary: '#FFFFFF',
+    secondary: '#191c42',
+    accent: '#151312',
+    background: '#F0F0F0',
+} 
+
+export default colors;
